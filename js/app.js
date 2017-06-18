69d20a3dd97d11706368f32e7f854b476e0985f8
Don <don@Dons-Mac-Pro.local>
2017-06-17 22:12:11 -0700

    functions removed, name prompt added. git push origin day2 git status

--- js/app.js
+++ js/app.js
@@ -3,48 +3,39 @@
 // first line of next prompt
 var intro = '';
 var score = 0;
-var answer = false;
 // question list
 var questions = ['First question: Does Don contain three letters?', 'Second question: Does Don start with a \'D\'', 'Third question: Does Don end with an \'n\'?', 'Fourth question: Does Don contain a vowel?', 'Fifth question: Is Don a palindrome?'];
 // answer key
-var correct = ['y', 'y', 'y', 'y', 'n'];
+var answerKey = ['y', 'y', 'y', 'y', 'n'];
+var summary = '';
+var guess = null;
+var myName;
 
-function checkAnswer(guess) {
-  // returns first letter if code is valid, false if not
-  var temp = guess.toLowerCase();
-  if (temp && (temp == 'yes' || temp == 'y' || temp == 'no' || temp == 'n')) {
-    temp = guess.slice(0, 1);
-  } else {
-    temp = false;
-  }
-  return(temp);
+while (!myName) {
+  myName = prompt('Hi there, welcome to my page!  What is your name?');
+  console.log('Name: ' + myName);
 }
 
-function checkScore(guess, solution) {
-  // verifies if score is correct, updates score and first line of next prompt
-  var message = '';
-  if (guess == solution) {
-    score ++;
-    message = 'Correct, your score is now ' + score + '.\n\n';
-  } else {
-    score --;
-    message = 'Wrong, your score is now ' + score + '.\n\n';
-  }
-  return(message);
-};
+intro = 'I\'m glad you finally showed up, ' + myName + '. Let\'s play a game to see how well you know Don.\n\n';
 
 for (var i = 0; i < 5; i++) {
-  answer = false;
-  while (!answer) {
-    var question = questions[i];
-    answer = checkAnswer(prompt(intro + question));
-    console.log(question + ' ' + answer);
-    if (answer) {
-      intro = checkScore(answer, correct[i]);
-    } else {
-      intro = 'That\'s not a valid response!\n\n';
-    }
-  };
+  guess = null;
+  // code only escapes loop if you have an answer that can be found in the array)
+  while (!guess || ['yes', 'y', 'no', 'n'].indexOf(guess.toLowerCase()) == -1) {
+    guess = prompt(intro + questions[i]);
+    intro = 'That\'s not a valid response!  Answer "yes" or "no" only.\n\n';
+    console.log(questions[i] + ': ' + guess);
+  }
+  // valid answers are reduced to one letter and then compared to key
+  if (guess.toLowerCase().slice(0,1) == answerKey[i]){
+    score++;
+    intro = 'Correct!\n\n';
+  } else {
+    intro = 'Wrong!\n\n';
+  }
 }
 
-alert(intro + 'You should be very proud of yourself.');
+// since guess from previous question is 'y' or 'n', while loop starts automatically
+
+summary = myName + ', you scored ' + score + ' points total.  You should feel very proud of yourself.';
+alert(intro + summary);
